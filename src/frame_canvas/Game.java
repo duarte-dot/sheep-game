b8064b1f87c14a9688ab4f24806e01b087bd5586
Gabriel Duarte <gabrieldvr@outlook.com>
2024-02-26 09:21:44 -0300

    organizando código

--- src/frame_canvas/Game.java
+++ src/frame_canvas/Game.java
@@ -1,4 +1,5 @@
 package frame_canvas;
+
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -8,7 +9,6 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferStrategy;
@@ -18,43 +18,53 @@ import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 
-    public static JFrame frame;
-    private Thread thread;
-    private boolean isRunning = false;
+    // Variáveis de Dimensão da Janela
     private final int WIDTH = 160;
     private final int HEIGHT = 120;
     private final int SCALE = 3;
 
+    // Componentes Gráficos
     private BufferedImage image;
-
     private Spritesheet sheet;
     private BufferedImage[] player;
+
+    // Controle de Jogo
+    private Thread thread;
+    private boolean isRunning = false;
     private int frames = 0;
     private int maxFrames = 20;
     private int currAnimation = 0, maxAnimation = 2;
-    
-    private int playerX = 20; // posição inicial do jogador em X
-    private int playerY = 20; // posição inicial do jogador em Y
-    private int playerSpeed = 1; // velocidade de movimento do jogador
-    private int playerDirectionX = 1; // 1 para direita, -1 para esquerda
-    private int playerDirectionY = 1; // 1 para baixo, -1 para cima
-    
+
+    // Posição e Movimento do Jogador
+    private int playerX = 20;
+    private int playerY = 20;
+    private int playerSpeed = 1;
+    private int playerDirectionX = 1;
+    private int playerDirectionY = 1;
+
+    // Controle de Teclas Pressionadas
     private boolean rightPressed = false;
     private boolean leftPressed = false;
     private boolean upPressed = false;
     private boolean downPressed = false;
 
+    // Janela do Jogo
+    public static JFrame frame;
+
+    // Construtor
     public Game() {
+        // Inicialização dos Componentes Gráficos
         sheet = new Spritesheet("/spritesheet.png");
         player = new BufferedImage[2];
-        player[0] = sheet.getSprite(0,  0, 16, 16);
+        player[0] = sheet.getSprite(0, 0, 16, 16);
         player[1] = sheet.getSprite(16, 0, 16, 16);
-        this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-        initFrame();
+        this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
         image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         setFocusable(false);
+        initFrame();
     }
 
+    // Método para Inicialização da Janela do Jogo
     public void initFrame() {
         frame = new JFrame();
         frame.addKeyListener(new KeyAdapter() {
@@ -74,7 +84,7 @@ public class Game extends Canvas implements Runnable {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        
+
         frame.addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
@@ -88,6 +98,7 @@ public class Game extends Canvas implements Runnable {
         });
     }
 
+    // Métodos de Controle de Thread do Jogo
     public synchronized void start() {
         thread = new Thread(this);
         isRunning = true;
@@ -103,24 +114,27 @@ public class Game extends Canvas implements Runnable {
         }
     }
 
+    // Método Principal do Jogo
     public static void main(String args[]) {
         Game game = new Game();
         game.start();
     }
 
+    // Métodos de Atualização do Jogo
     public void tick() {
         frames++;
         if (frames > maxFrames) {
             frames = 0;
             currAnimation++;
-
             if (currAnimation >= maxAnimation) {
                 currAnimation = 0;
             }
         }
-        
-        // Atualiza a posição do jogador de acordo com as teclas pressionadas
-        // e verifica se atingiu os limites da janela
+
+        updatePlayerPosition();
+    }
+
+    private void updatePlayerPosition() {
         if (leftPressed) {
             playerX -= playerSpeed;
             if (playerX < 0) {
@@ -145,33 +159,94 @@ public class Game extends Canvas implements Runnable {
                 playerY = 0 - 8;
             }
         }
-        
-        // Verifica se o jogador está parado e define a animação de parado
         if (!leftPressed && !rightPressed && !upPressed && !downPressed) {
-            currAnimation = 0; // Define a animação para a sprite de parado
+            currAnimation = 0;
         }
     }
 
+    // Métodos de Renderização do Jogo
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
 
+        Graphics g = image.getGraphics();
+        g.setColor(new Color(19, 19, 19));
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        renderPlayer(g2);
+
+        g.dispose();
+        g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+        bs.show();
+    }
+    
+    public void drawPlayer(Graphics2D g2, int x, int y) {
+        g2.drawImage(player[currAnimation], x, y, null);
+    }
+    
+    public void drawReversedPlayer(Graphics2D g2, int x, int y) {
+    	AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+        tx.translate(-player[currAnimation].getWidth(null), 0);
+        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        BufferedImage reversedImage = op.filter(player[currAnimation], null);
+        g2.drawImage(reversedImage, x, y, null);
+    }
+
+    private void renderPlayer(Graphics2D g2) {
+        int oppositeX = (playerX < 0) ? playerX - WIDTH : playerX - WIDTH;
+        int oppositeY = (playerY < 0) ? playerY + HEIGHT : playerY - HEIGHT;
+
+        if (playerX < 0 || playerX + 16 > WIDTH) {
+            if (playerDirectionX == 1) {
+            	drawPlayer(g2, playerX, playerY);
+            } else {
+                drawReversedPlayer(g2, oppositeX, playerY);
+            }
+
+            if (playerDirectionX == -1) {
+            	drawReversedPlayer(g2, playerX, playerY);
+            } else {
+            	drawPlayer(g2, oppositeX, playerY);
+            }
+        } else {
+            if (playerDirectionX == -1) {
+            	drawReversedPlayer(g2, playerX, playerY);
+            } else {
+            	drawPlayer(g2, playerX, playerY);
+            }
+        }
+
+        if (playerY < 0 || playerY + player[currAnimation].getHeight() > HEIGHT) {
+            if (playerDirectionX == -1) {
+            	drawReversedPlayer(g2, playerX, oppositeY);
+            } else {
+            	drawPlayer(g2, playerX, oppositeY);
+            }
+        }
+    }
+
+    // Métodos de Controle de Teclado
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP) {
-            // Movimento para cima
             upPressed = true;
-            playerDirectionY = -1; // Define a direção vertical para cima
+            playerDirectionY = -1;
         } else if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) {
-            // Movimento para baixo
             downPressed = true;
-            playerDirectionY = 1; // Define a direção vertical para baixo
+            playerDirectionY = 1;
         } else if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
-            // Movimento para a esquerda
             leftPressed = true;
-            playerDirectionX = -1; // Define a direção horizontal para esquerda
+            playerDirectionX = -1;
         } else if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
-            // Movimento para a direita
             rightPressed = true;
-            playerDirectionX = 1; // Define a direção horizontal para direita
+            playerDirectionX = 1;
         }
     }
 
@@ -188,93 +263,8 @@ public class Game extends Canvas implements Runnable {
             rightPressed = false;
         }
     }
-    
 
-
-    public void render() {
-        BufferStrategy bs = this.getBufferStrategy();
-        if (bs == null) {
-            this.createBufferStrategy(3);
-            return;
-        }
-
-        Graphics g = image.getGraphics();
-        g.setColor(new Color(19, 19, 19));
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-
-        Graphics2D g2 = (Graphics2D) g;
-
-        // Verifica se a parte da ovelha está fora da tela na horizontal
-     // Calcula a posição X da parte oposta da ovelha
-        int oppositeX = (playerX < 0) ? playerX - WIDTH : playerX - WIDTH;
-        
-        if (playerX < 0 || playerX + 16 > WIDTH) {
-        	System.out.println("menor que 0 ou maior que width");
-            if (playerDirectionX == 1) { // Direita
-//            	System.out.println("direita 1");
-                g2.drawImage(player[currAnimation], playerX, playerY, null);
-            } else { // Esquerda
-            	System.out.println("esquerda 1");
-            	 // Inverte a imagem horizontalmente
-                AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-                tx.translate(-player[currAnimation].getWidth(null), 0);
-                AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-                BufferedImage reversedImage = op.filter(player[currAnimation], null);
-                g2.drawImage(reversedImage, oppositeX, playerY, null);
-            }
-
-            
-            // Desenha a parte oposta da ovelha
-            if (playerDirectionX == -1) { // Esquerda
-            	 AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-               tx.translate(-player[currAnimation].getWidth(null), 0);
-               AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-               BufferedImage reversedImage = op.filter(player[currAnimation], null);
-               g2.drawImage(reversedImage, playerX, playerY, null);
-            } else { // Direita
-                g2.drawImage(player[currAnimation], oppositeX, playerY, null);
-            }
-        } else {
-            // Desenha a ovelha normalmente sem considerar a parte oposta
-            if (playerDirectionX == -1) { // Esquerda
-            	System.out.println("esquerda 2");
-            	 // Inverte a imagem horizontalmente
-                AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-                tx.translate(-player[currAnimation].getWidth(null), 0);
-                AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-                BufferedImage reversedImage = op.filter(player[currAnimation], null);
-                g2.drawImage(reversedImage, playerX, playerY, null);            
-                } else { // Direita
-//                	System.out.println("direita 2");
-                g2.drawImage(player[currAnimation], playerX, playerY, null);
-            }
-        }
-
-        // Verifica se a parte da ovelha está fora da tela na vertical
-        if (playerY < 0 || playerY + player[currAnimation].getHeight() > HEIGHT) {
-            // Calcula a posição Y da parte oposta da ovelha
-            int oppositeY = (playerY < 0) ? playerY + HEIGHT : playerY - HEIGHT;
-            if (playerDirectionX == -1) { // Esquerda
-            	System.out.println("esquerda 2");
-            	// Inverte a imagem horizontalmente
-                AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-                tx.translate(-player[currAnimation].getWidth(null), 0);
-                AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-                BufferedImage reversedImage = op.filter(player[currAnimation], null);
-                g2.drawImage(reversedImage, playerX, oppositeY, null);            
-                } else { // Direita
-//                	System.out.println("direita 2");
-                g2.drawImage(player[currAnimation], playerX, oppositeY, null);
-            }
-        }
-
-        g.dispose();
-        g = bs.getDrawGraphics();
-        g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
-        bs.show();
-    }
-
-
+    // Método de execução da Thread
     public void run() {
         long lastTime = System.nanoTime();
         double amountOfTicks = 60.0;
@@ -283,9 +273,9 @@ public class Game extends Canvas implements Runnable {
 
         int frames = 0;
         double timer = System.currentTimeMillis();
-        while(isRunning) {
+        while (isRunning) {
             long now = System.nanoTime();
-            delta+= (now - lastTime) / ns;
+            delta += (now - lastTime) / ns;
 
             lastTime = now;
 
